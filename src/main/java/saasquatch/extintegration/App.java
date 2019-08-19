8a313b9e864a60e1d2d96a2dcf38d910556bc75b
slisaasquatch <sli@saasquat.ch>
2019-08-19 11:06:29 -0700

    SL: empty project

---
+++ src/main/java/saasquatch/extintegration/App.java
@@ -0,0 +1,13 @@
+package saasquatch.extintegration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
