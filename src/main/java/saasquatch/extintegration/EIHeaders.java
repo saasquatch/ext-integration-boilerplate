22b9a9f6c2f5813f575aa6fb40b52437fa988eec
slisaasquatch <sli@saasquat.ch>
2020-03-17 14:27:25 -0700

    SL: deleted unnecessary headers

--- src/main/java/saasquatch/extintegration/EIHeaders.java
+++ src/main/java/saasquatch/extintegration/EIHeaders.java
@@ -1,30 +1,20 @@
 package saasquatch.extintegration;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
-import javax.annotation.Nonnull;
+import com.google.common.collect.ImmutableSortedMap;
 import com.google.common.net.HttpHeaders;
 
 public class EIHeaders {
 
-  public static Map<String, String> getGlobalHeaders(@Nonnull Collection<String> frameSrc) {
-    final List<String> csp = new ArrayList<>();
-    csp.add("default-src https: 'unafe-inline' 'unsafe-eval'");
-    if (!frameSrc.isEmpty()) {
-      csp.add("frame-src " + String.join(" ", frameSrc));
-    }
-    final Map<String, String> headers = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-    headers.put(HttpHeaders.STRICT_TRANSPORT_SECURITY, "max-age=31536000");
-    headers.put(HttpHeaders.CONTENT_SECURITY_POLICY, String.join("; ", csp));
-    headers.put(HttpHeaders.X_FRAME_OPTIONS, "SAMEORIGIN");
-    headers.put(HttpHeaders.X_CONTENT_TYPE_OPTIONS, "nosniff");
-    headers.put(HttpHeaders.REFERRER_POLICY, "no-referrer-when-downgrade");
-    headers.put("Feature-Policy", "'none'");
-    return Collections.unmodifiableMap(headers);
+  private static final Map<String, String> GLOBAL_HEADERS =
+      ImmutableSortedMap.<String, String>orderedBy(String.CASE_INSENSITIVE_ORDER)
+          .put(HttpHeaders.STRICT_TRANSPORT_SECURITY, "max-age=31536000")
+          .put(HttpHeaders.X_FRAME_OPTIONS, "SAMEORIGIN")
+          .put(HttpHeaders.X_CONTENT_TYPE_OPTIONS, "nosniff")
+          .build();
+
+  public static Map<String, String> getGlobalHeaders() {
+    return GLOBAL_HEADERS;
   }
 
 }
